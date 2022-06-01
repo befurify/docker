ce23cba2b63823e95e2be1f13853e27669580f03
befurify <89641895+befurify@users.noreply.github.com>
2022-06-01 17:13:37 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "241aebaf-3840-42d2-b47e-3da5a109c5c6",
                         "alterId": 0
                     }
                 ],
